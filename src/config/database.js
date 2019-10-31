856ca4cb0a86f77ee7e6258912f18aba92772ce5
Thiago Marinho <tgmarinho@gmail.com>
2019-10-31 15:00:01 -0400

    Testes com Node - Aula 05 - teste de criação de usuário

--- src/config/database.js
+++ src/config/database.js
@@ -6,7 +6,8 @@ module.exports = {
   username: process.env.DB_USER,
   password: process.env.DB_PASS,
   database: process.env.DB_NAME,
-  storage: './__tests__/database.sqlite',
+  storage: './__tests__/databese.sqlite',
+  logging: false,
   define: {
     timestamps: true,
     underscored: true,
