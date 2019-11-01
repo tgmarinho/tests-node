268fc1537febcf9c78798b06ae10489eb1c67666
Thiago Marinho <tgmarinho@gmail.com>
2019-11-01 10:40:52 -0400

    Testes com Node - Aula 08 - Gerando dados aleatórios

--- __tests__/integration/user.test.js
+++ __tests__/integration/user.test.js
@@ -1,8 +1,8 @@
 import request from 'supertest';
 import bcrypt from 'bcryptjs';
 import app from '../../src/app';
-import User from '../../src/app/models/User';
 import truncate from '../util/truncate';
+import factory from '../factories';
 
 describe('User', () => {
   beforeEach(async () => {
@@ -10,9 +10,7 @@ describe('User', () => {
   });
 
   it('should encrypt user password when neh user is created', async () => {
-    const user = await User.create({
-      name: 'Thiago Marinho',
-      email: 'tgmarinho@gmail.com',
+    const user = await factory.create('User', {
       password: '123456',
     });
 
@@ -22,33 +20,25 @@ describe('User', () => {
   });
 
   it('should be able to register', async () => {
+    const user = await factory.attrs('User');
+
     const response = await request(app)
       .post('/users')
-      .send({
-        name: 'Thiago Marinho',
-        email: 'tgmarinho@gmail.com',
-        password: '123456',
-      });
+      .send(user);
 
     expect(response.body).toHaveProperty('id');
   });
 
   it('should not be able to register with duplicate email', async () => {
+    const user = await factory.attrs('User');
+
     await request(app)
       .post('/users')
-      .send({
-        name: 'Thiago Marinho',
-        email: 'tgmarinho@gmail.com',
-        password: '123456',
-      });
+      .send(user);
 
     const response = await request(app)
       .post('/users')
-      .send({
-        name: 'Thiago Marinho',
-        email: 'tgmarinho@gmail.com',
-        password: '123456',
-      });
+      .send(user);
 
     expect(response.status).toBe(400);
   });
